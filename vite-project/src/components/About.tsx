2a3299f57e365c0a455f96fcc68957bc86aff093
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-09 21:17:34 -0500

    added some mores stlye

--- vite-project/src/components/About.tsx
+++ vite-project/src/components/About.tsx
@@ -3,30 +3,13 @@ const About:React.FC = () => {
 
   return (
     <a href="#"className="group relative inline-block overflow-hidden rounded border border-neutral-100 bg-neutral-100  px-12 py-3 text-sm font-medium text-slate-800 focus:outline-none focus:ring active:bg-indigo-600 active:text-white">
-        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-green-600 transition-all duration-200 group-hover:w-full"></span>
-        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-green-600 transition-all duration-200 group-hover:h-full"></span>
-        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-green-600 transition-all duration-200 group-hover:w-full"></span>
-        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-green-600 transition-all duration-200 group-hover:h-full"></span>
+        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-green-400 transition-all duration-200 group-hover:w-full"></span>
+        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-green-400 transition-all duration-200 group-hover:h-full"></span>
+        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-green-400 transition-all duration-200 group-hover:w-full"></span>
+        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-green-400 transition-all duration-200 group-hover:h-full"></span>
         About
     </a>
   )
-  {/* 
-  return (
-    <>
-        <button className="bg-none cursor-pointer inline-block flex-shrink-0 text-2xl py-1 px-1 relative text-black no-underline z-10 font-bold
-            before:bg-white before:h-full before:absolute before:w-full before:-z-10 before:top-3 before:right-3
-            after:border-gray-800 after:border-solid after:border-2 after:h-full after:opacity-100 after:absolute after:top-0 after:right-0 after:w-full
-            hover:before:translate-x-3 hover:before:-translate-y-3
-            hover:after:-translate-x-3 hover:after:translate-y-3
-            hover:before:transition-transform hover:before:duration-500 hover:before:ease-in
-            hover:after:transition-transform hover:after:duration-500 hover:after:ease-in
-            after:transition-transform after:duration-500
-            before:transition-transform before:duration-500
-            ">About
-        </button>
-    </>
-  )
-*/}
 }
 
 export default About
--- vite-project/src/components/StartButton.tsx
+++ vite-project/src/components/StartButton.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 const StartButton: React.FC = () => {
   return (
-    <div className="flex justify-center">
-        <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
-          <div className="absolute inset-0 w-3 bg-green-700 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-          <span className="relative text-black group-hover:text-white">Hover me!</span>
-      </button>
-    </div>
+    <div className="flex flex-col justify-center items-center w-90 h-40 bg-neutral-100">
+    <button
+        className="relative text-center h-20 w-60 transition-all duration-500
+            before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-neutral-200 before:transition-all
+            before:duration-300 before:opacity-10 before:hover:opacity-0 before:hover:scale-50
+            after:absolute after:top-0 after:left-0 after:w-full after:h-full after:opacity-0 after:transition-all after:duration-300
+            after:border after:border-white/50 after:scale-125 after:hover:opacity-100 after:hover:scale-100"
+    >
+        <span className="relative text-black uppercase font-thin">Hover me</span>
+    </button>
+</div>
   )
 }
 
