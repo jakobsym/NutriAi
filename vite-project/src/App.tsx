28fe4237ab2b26643346ce045fbec0bf588d3d35
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-14 11:42:29 -0500

    implement routing for quiz page.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,29 @@
+import { BrowserRouter,
+  Route,
+  Routes,} from 'react-router-dom';
+  
 import './App.css'
 import LandingPage from './pages/LandingPage/LandingPage'
+import QuizPage from './pages/quiz_page/QuizPage'
 
 function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/quiz/*" element={<QuizPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+
+  {/* 
   return (
     <>
       <LandingPage />
     </>
   )
+  */}
 }
 
 export default App
