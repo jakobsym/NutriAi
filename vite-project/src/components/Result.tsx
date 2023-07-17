3b3668e53415ede3495e423a676e6baf10121c46
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-17 13:57:51 -0500

    pass userChoices[] as prop to Result page to prompt llm using provided choices

---
+++ vite-project/src/components/Result.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Quiz.css'
+
+/* Integrate API within this function */
+const Result = (props: {userChoice: string[]}) => {
+  return (
+    <div className="result">
+            <h3>Example Prompt:</h3>
+            <div>
+                <p>
+                    {"I want to " + props.userChoice[0] + " doing " + props.userChoice[2] + ". I weigh " + props.userChoice[1]
+                    + ". What should my daily caloric intake be?" }
+                </p>
+            </div>
+            
+    </div>
+  )
+}
+
+export default Result
