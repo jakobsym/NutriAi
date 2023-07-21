f37f3bcd0c7fed3cf48fcd9716a78b4a1e2b8567
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-21 16:01:37 -0500

    installed needed dependencies

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+import react from '@vitejs/plugin-react-swc';
+
+dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +11,4 @@ export default defineConfig({
   server: {
     port:3000
   },
-})
+});
