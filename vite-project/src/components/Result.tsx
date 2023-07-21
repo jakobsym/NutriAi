40240d9804a9d4fd5ab6608bb83349a6f47954b3
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-21 14:30:22 -0500

    implemented openAI API. Users quiz choices are passed to API and response is returned as a li element which is displayed to user. Need to finalize styling

--- vite-project/src/components/Result.tsx
+++ vite-project/src/components/Result.tsx
@@ -1,10 +1,11 @@
 import '../Quiz.css'
 import { useEffect, useState } from "react";
+import  generatePrompt from "../api/generate.js";
 
 /* Integrate API within this function (index.js)*/
 const Result = (props: {userChoice: string[]}) => {
-    const [result, setResult] = useState();
-    const [dispResult, setDispResult] = useState(false);
+    const [result, setResult] = useState<String[]>();
+    const [dispResult, setDispResult] = useState<boolean>(false);
 
     const handleClick = () => {
         setDispResult(true);
@@ -17,32 +18,12 @@ const Result = (props: {userChoice: string[]}) => {
         }
     }, [dispResult]);
 
-    const userInput:string = "Im a " + props.userChoice[0] + " that weighs about " + props.userChoice[1] + ". I want to " + 
-        props.userChoice[2] + " doing " + props.userChoice[3] + ". Explain very breifly how I can acheive my goal."
+    const userInput :string = "Im a " + props.userChoice[0] + " that weighs about " + props.userChoice[1] + ". I want to " + 
+        props.userChoice[2] + " doing " + props.userChoice[3] + ". Return a response using only 5 bullet points that will help me acheive my goal:" ;
     
 
-    async function displayResult(): Promise<any>{
-        //setDispResult(true); //boolean val not changed with useState() as its asyncho need useEffect()
-        //event.preventDefault();
-        console.log("dispResult = "+ dispResult)
-        try {
-            const response = await fetch("../api/generate", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ input: userInput})
-            });
-
-            const data = await response.json();
-            if (response.status !== 200){
-                throw data.error || new Error(`Request failed with status ${response.status}`);
-            }
-            setResult(data.result);
-        } catch(error : any){
-            console.error(error);
-            alert(error.message)
-        }
+    async function displayResult() {
+        setResult(await generatePrompt(userInput));
     }
 
     return (
@@ -51,9 +32,15 @@ const Result = (props: {userChoice: string[]}) => {
             <h1>Personal Trainer:</h1>
             <button onClick={handleClick}> Display results.</button>
         </div>
-    
         <div>
-            <p>{result}</p> {/* This is response from API */}
+            <ul>
+                {result && result.map((point, index) => (
+                    <li key={index}>
+                        {console.log(point.trim())!}
+                        {point.trim()}
+                    </li>
+                ))}  {/* This is response from API */}
+            </ul>
         </div>
         </> 
     )
--- vite-project/src/components/StartButton.tsx
+++ vite-project/src/components/StartButton.tsx
@@ -9,7 +9,12 @@ const StartButton: React.FC = () => {
   }
   return (
     <div className="flex flex-col justify-center items-center w-90 h-40">
-      <button onClick={handleClick} className="bg-gradient-to-b w-max mx-auto text-black font-semibold from-neutral-100 active:opacity-40 to-neutral-200 px-10 py-3 rounded-2xl shadow-black shadow-md border-b-4 hover border-neutral-200 hover:shadow-sm transition-all duration-500">Get started</button>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Choose plan
+        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+      </button>
     </div>
   )
   {/* 
