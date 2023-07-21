f1d77104c0d080425dd98ce4ae716b7527373f1a
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-21 14:34:00 -0500

    adding response generator code to project

--- vite-project/src/api/generate.js
+++ vite-project/src/api/generate.js
@@ -1,16 +1,32 @@
 import {Configuration, OpenAIApi} from "openai"
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: OPENAI_API_KEY,
   });
 
 const openai = new OpenAIApi(configuration);
 
-export default async function(req, res) {
-  /* Implement API call here */
+export default async function generatePrompt(choice) {
+  //console.log(choice);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: choice,
+    max_tokens: 300,
+    temperature: 1.0,
+  });
 
+  const userResult = response.data.choices[0].text.trim();
+  const resultList = userResult.split("\n\n");
+
+  console.log(resultList)
+  return resultList;
+}
+
+
+/*
+export default async function(req, res) {
   // Ensure configuration of API key is initialized
-  if (! configuration.apiKey) {
+  if (!configuration.apiKey) {
     res.status(500).json({
       error: {
         message: "OpenAI key is not configured."
@@ -32,9 +48,11 @@ export default async function(req, res) {
   try{
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(input),
+      prompt: genPrompt(input),
       temperature: 0.6,
+      maxTokens: 500,
     });
+    
     res.status(200).json({ result: completion.data.choices[0].text});
   } catch(error){
     if (error.response){
@@ -51,24 +69,4 @@ export default async function(req, res) {
     }
   }
 }
-
-// `choice` === userChoice[]: string[]
-function genPrompt(choice){
-  return `Based on provided prompts use 5 brief prompts to act as a personal trainer
-    choice: Im a male that weighs 135lbs. I want to maintain weight doing weight training.
-    answer: Caloric Surplus: Consume more calories than your body burns in a day to create a surplus for weight gain.
-      High-Protein Diet: Include sufficient protein in your meals to support muscle growth and repair.
-      Progressive Overload: Focus on compound weight training exercises and gradually increase the resistance to stimulate muscle growth.
-      Consistent Training: Train with weights 3-4 times a week, allowing adequate rest for muscle recovery.
-      Patience and Monitoring: Be patient with your progress and monitor weight, strength gains, and overall changes to adjust your approach if necessary.
-
-    choice: Im a female that weighs 100lbs. I want to gain weight doing a little bit of everything.
-    answer: Increase Caloric Intake: Consume more calories than your body burns in a day by adding nutrient-dense foods to your meals and snacks.
-      Strength Training: Incorporate resistance exercises like weight lifting or bodyweight exercises to build muscle mass and add healthy weight.
-      Balanced Diet: Ensure your diet includes a good balance of carbohydrates, proteins, and healthy fats to support overall weight gain and fuel your activities.
-      Regular Meals and Snacks: Eat frequent, balanced meals, and include snacks throughout the day to maintain a steady supply of nutrients and energy.
-      Stay Active: Engage in moderate cardiovascular activities like walking, cycling, or dancing to support a healthy appetite and overall well-being.
-  
-    choice: ${choice}
-    answer:`;
-}
+*/
