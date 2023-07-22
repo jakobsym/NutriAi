8272996c676b2ebfe7f88b54957e7dc67b38d0f0
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-22 17:57:22 -0500

    fixed placement, and style/desgin of start button to fix general design

--- vite-project/src/components/StartButton.tsx
+++ vite-project/src/components/StartButton.tsx
@@ -8,11 +8,11 @@ const StartButton: React.FC = () => {
     //console.log("Clicked: buttonClick = " + buttonClick);
   }
   return (
-    <div className="flex flex-col justify-center items-center w-90 h-40">
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Choose plan
+    <div className="flex flex-col justify-center items-center w-90 h-40 mb">
+      <button type="button" className="text-white bg-neutral-100 hover:bg-neutral-100 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-black dark:hover:bg-neutral-800">
+        Get Started
         <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+          <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
         </svg>
       </button>
     </div>
