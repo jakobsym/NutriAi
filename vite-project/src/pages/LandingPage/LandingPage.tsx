0bf68ebc530c70a66986c982d796a97e1bf71c9c
jakobsym <jakob.symchych@my.utsa.edu>
2023-07-29 09:31:22 -0500

    added footer to landingpage

--- vite-project/src/pages/LandingPage/LandingPage.tsx
+++ vite-project/src/pages/LandingPage/LandingPage.tsx
@@ -7,8 +7,10 @@ const LandingPage = () => {
     <>
         <Header />
         <HeroSection />
+        <div className="h-[50px] bg-[#000000] opacity-90 fixed bottom-0 left-0 z-20 w-full flex flex-row items-center justify-evenly">
+          <p className="text-white/80 text-base md:text-lg font-semibold md:leading-[60px] whitespace-nowrap flex flex-row">Footer section TBA</p>
+        </div>
     </>
   )
 }
-
 export default LandingPage
