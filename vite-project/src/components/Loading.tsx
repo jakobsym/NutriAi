90b02f20d2278725a2c375493e9273f4e6fa7e9b
jakobsym <jakob.symchych@my.utsa.edu>
2023-08-02 13:17:27 -0500

    fixed bad code structure within component

--- vite-project/src/components/Loading.tsx
+++ vite-project/src/components/Loading.tsx
@@ -8,11 +8,9 @@ const Loading: React.FC<LoadingProps> = ({size}) => {
   return (
     <div className="w-[50%] h-10 space-x-4 flex items-center justify-center">
         <div style={{ width: `${size}px`, height: `${size}px` }} className="animate-spin ">
-        <div className="h-full w-full border-4 border-t-zinc-800
-       border-b-black rounded-[50%]">
-      </div>
+            <div className="h-full w-full border-4 border-t-zinc-800 border-b-black rounded-[50%]"></div>
+        </div>
     </div>
-  </div>
   )
 }
 
