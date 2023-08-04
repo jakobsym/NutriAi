d0d16b8c912c954fff49fc9504e5a0098fb0d6bb
jakobsym <jakob.symchych@my.utsa.edu>
2023-08-04 14:27:27 -0500

    added error handling to api generate function. Handled error when fetching api through front end using params of Configuration()

--- vite-project/src/api/generate.js
+++ vite-project/src/api/generate.js
@@ -1,53 +1,54 @@
 import {Configuration, OpenAIApi} from "openai";
-//TODO: Implement error handling
+
 const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
 
 const openai = new OpenAIApi(configuration);
+let resultList; // store output in formatted maner
 
-export default async function generatePrompt(choice) {
-  //console.log(choice);
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: choice,
-    max_tokens: 300,
-    temperature: 1.0,
-  });
+// Handle ' Refused to set unsafe header "User-Agent" ' error
+configuration.baseOptions.headers = {
+  'Authorization': 'Bearer ' + configuration.apiKey
+}
 
-  const userResult = response.data.choices[0].text.trim();
-  const resultList = userResult.split("\n\n");
+export default async function generatePrompt(choice, res){
+  //console.log(choice)
 
-  console.log(resultList)
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured properly."
+      }
+    })
+  }
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: choice,
+      max_tokens: 420,
+      temperature: 1.0,
+    })
+
+    const userResult = response.data.choices[0].text.trim();
+    resultList = userResult.split("\n\n");
+
+  } catch (error) {
+
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "Error occured during request.",
+        }
+      });
+    }
+  }
+  
+  //console.log(resultList)
   return resultList;
 }
-
-
-/*
-// Fix to 'unsafe header "User-Agent" ' error
-
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + String(openAIKey)
-  },
-  body: JSON.stringify({
-    'prompt': prompt,
-    'temperature': 0.1,
-    'max_tokens': Math.floor(fileLength/2),
-    'top_p': 1,
-    'frequency_penalty': 0,
-    'presence_penalty': 0.5,
-    'stop': ["\"\"\""],
-  })
-};
-fetch('https://api.openai.com/v1/engines/code-davinci-001/completions', requestOptions)
-    .then(response => response.json())
-    .then(data => {
-      # Do something with data
-  }).catch(err => {
-    console.log("Ran out of tokens for today! Try tomorrow!");
-  });
-}
-*/
